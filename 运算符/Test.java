73b23b4f8bd7a101a0fd0132a583c09aa69273dd
Your Name <you@example.com>
2020-12-09 13:09:22 +0800

    .

---
+++ 运算符/Test.java
@@ -0,0 +1,24 @@
+public class Test
+{
+  public static void main(String args[])
+    {
+	  int a=7+2;      //a=9
+	  int b=a*2;	  //b=18
+	  int c=b/9;      //c=2
+	  int d=-a;       //d=-9
+	  int e=d%2;      //e=-1
+	  double f=17.5%4;//f=1.5
+	  int i=2;
+	  int j=i++;      //i=3，j=2
+          int k=++i;      //i=4，k=4
+	  System.out.println("a="+a);
+	  System.out.println("b="+b);
+	  System.out.println("c="+c);
+	  System.out.println("d="+d);
+	  System.out.println("e="+e);
+	  System.out.println("f="+f);
+	  System.out.println("i="+i);
+	  System.out.println("j="+j);
+	  System.out.println("k="+k);
+	}
+}
