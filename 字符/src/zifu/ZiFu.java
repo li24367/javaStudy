4a402252a09ef7ed1204e45228ccd80db2074335
li24367 <3088147516@qq.com>
2020-12-10 21:14:28 +0800

    字符

---
+++ 字符/src/zifu/ZiFu.java
@@ -0,0 +1,23 @@
+package zifu;
+
+public class ZiFu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String a=new String("我相信，");
+      String b="我一定能学会java";
+      String c=a+b;
+      System.out.println(c);
+      
+      String e="中国江";
+      
+      System.out.println(e.concat("苏，大美江南"));
+      
+      String g="我恨java";
+      String h=g.replace('恨', '爱');
+      System.out.println(h);
+      
+      
+      }
+
+}
