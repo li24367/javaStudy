123b90ec3f8ee85e7f54e10eb6d25b14dc770163
li24367 <3088147516@qq.com>
2020-12-11 16:08:54 +0800

    字符2

---
+++ zifu2.java
@@ -0,0 +1,27 @@
+package zifu2;
+
+public class zifu2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method st
+       String x="重庆位于中国的中西部，是西部文化中心！";
+       String y=x.substring(8);
+       String z=x.substring(12,16);
+       System.out.println(y);
+       System.out.println(z);
+       
+       String a="I DON'T KNOW";
+       String b=a.toLowerCase();
+       String c="i don't know";
+       String d=c.toUpperCase();
+       System.out.println(b);
+       System.out.println(d);
+       
+       String e="          我一定能学会盲打！";
+       String f=e.trim();
+       System.out.println(f);
+    		   
+    		   
+	}
+
+}
